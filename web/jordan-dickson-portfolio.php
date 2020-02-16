78e312c46cb731221b9b5da5f99405d022250371
Jordan Dickson <jfdickson25@gmail.com>
2020-02-16 00:10:17 -0700

    Hover change

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -102,7 +102,7 @@
         <!-- Portfolio Item 2 -->
         <div class="col-md-6 col-lg-4">
           <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100 bg-secondary" style="opacity:.5">
+            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100 bg-secondary" hover="opacity:.5">
               <div class="portfolio-item-caption-content text-center text-white" hover="opacity:5">
                 <i class="fas fa-plus fa-3x"></i>
               </div>
