dc82f39fe893dc1b76e20d445d7a661a00dc7203
Jordan Dickson <jfdickson25@gmail.com>
2020-02-16 00:22:02 -0700

    Deleted hover

--- web/jordan-dickson-portfolio.php
+++ web/jordan-dickson-portfolio.php
@@ -103,7 +103,7 @@
         <div class="col-md-6 col-lg-4">
           <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
             <div class="bg-secondary portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div class="portfolio-item-caption-content text-center text-white" hover="opacity:5">
+              <div class="portfolio-item-caption-content text-center text-white">
                 <i class="fas fa-plus fa-3x"></i>
               </div>
             </div>
