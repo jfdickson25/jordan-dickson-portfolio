1c79bb230af60bb415f93bf9c4f4f30fdbd085c8
Jordan Dickson <jfdickson25@gmail.com>
2020-04-07 16:39:45 -0600

    seperated database items from local

---
+++ web/personal-projects/web/dickson-cookbook/db/soups.sql
@@ -0,0 +1,17 @@
+/* Soups */
+  INSERT INTO category (category_name) VALUES ('Soups');
+  
+  INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Zero Points Soup from Weight Watchers', 
+    '⅔ cup sliced carrot
+    ½ cup diced onion
+    2 garlic cloves, minced or ¼ tsp garlic powder
+    3 cups broth (chicken, beef or vegetable)
+    1 ½ to 2 cups diced green cabbage
+    1 can green beans with liquid
+    1 Tblsp tomato paste
+    ½ tsp dried basil
+    ¼ tsp dried oregano
+    ¼ tsp salt
+    1 cup diced zucchini', 
+    'Combine everything into a pot, bring to a boil, cover, reduce heat to low and simmer for about 15 minutes.  
+    42 calories per serving if you are counting this soup.  Makes 4 servings', 3);
---
+++ web/personal-projects/web/dickson-cookbook/db/vegetables.sql
@@ -0,0 +1,248 @@
+/* Vegetables */
+  INSERT INTO category (category_name) VALUES ('Vegetables');
+  
+  INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Zucchini Puffed Cheese', 
+    '½ cup Swiss cheese or cheddar 
+    2 Tbs. grated Parmesan cheese 
+    ½ cup bread crumbs 
+    2 Tbs. margarine, melted 
+    1 cup all-purpose flour 
+    ½ tsp. salt 
+    ¼ tsp. double-acting baking powder 
+    1 1/3 cups margarine, melted 
+    4 Tbs. milk 
+
+    Filling 
+    3 cups zucchini, sliced 
+    ½ cup boiling water 
+    2 tsp. salt 
+    2 large eggs, separated 
+    1 ½ cups sour cream 
+    2 Tbs. all-purpose flour 
+    2 Tbs. chives 
+    1/8 tsp. black pepper 
+    ¼ tsp. cream of tarter', 
+    'Combine cheeses; reserve ¼ cup. Combine remaining cheeses with breadcrumbs and 2 Tbs. melted margarine. 
+    Set aside. Combine flour, salt and baking powder in mixing bowl. Cut in 1/3 cup margarine and reserved ¼ cup cheeses 
+    until mixture is size of small peas. Sprinkle milk over flour mixture, stirring with fork until dough is moist enough 
+    to hold together. Form into a ball then flatten to ½ inch thickness. Smooth the edges with your hands. Roll out on floured surface to 
+    a circle 1 ½ inches larger than an inverted 9-inch pie plate. Fit into pan, fold edges under then flute. Chill while preparing filling. 
+
+    Filling 
+    Cook zucchini in water and 1 tsp. salt in covered saucepan for 5 minutes or until tender. Drain well. Beat egg yolks slightly: add sour cream, 
+    1 tsp. salt, flour, chives and pepper. In large mixing bowl, beat egg whites with cream of tartar until stiff but not dry. Fold into sour cream 
+    mixture. Place half of zucchini in pastry lined pan. Cover with half of sour cream mixture. Top with remaining zucchini then with remaining sour 
+    cream mixture. Sprinkle with cheese breadcrumb mixture. Bake in preheated 400˚ oven for 10 minutes. Lower heat to 325 and bake 40-50 minutes longer 
+    until knife inserted halfway between center and edge comes out clean. Serves 6', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Swiss Corn Bake', 
+    'NA', 
+    'Cook 3 cups fresh-cut corn in 1 cup boiling water for 2-3 minutes, or till just tender; OR cook two 9 ounce packages frozen corn according to package directions; drain well.
+    Combine corn, one 6 ounce can evaporated milk, 1 beaten egg, 2 tablespoons finely chopped onion, ½ tsp salt, dash pepper, and ¾ cup shredded Swiss Cheese.
+    Pour into an 8x8 square pan.  Toss ½ cup soft bread crumbs with 1 Tablespoon melted butter and ¼ cup shredded Swiss cheese.  Sprinkle over top.  Bake at 350 for 25 minutes. 
+    Serves 6', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Southern Style Black Eyed Peas', 
+    '12 ounce bag frozen black eyed peas
+    2 cups chicken stock
+    ½ small onion
+    1 Tblsp butter
+    ¼ cup cubed ham (or bacon)
+    1 tsp salt', 
+    'Combine all ingredients into a pot and bring to a boil.  Let simmer for 45 mins.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Pumpkin Puree', 
+    'NA', 
+    'You will want to start with a 2-4 pound sugar pumpkin (they work best for roasting because they are smaller than carving pumpkins, 
+    but really, any old pumpkin will do).  Cut in half, scoop out the seeds and stringy stuff and lay them face down on a cookie sheet.  
+    Place the cookie sheet in the oven at 350-375.  Depending on the size of the pumpkins it will take 45-60 minutes to roast.  You will 
+    know when they are done because the skin of the pumpkin will get wrinkly.  If you can easily stick a fork in it, then it’s probably done.  
+    You want it to be soft, but not gooey.  Set aside until it’s cool enough to handle, then peel the skin off the pumpkin and put the cooked 
+    flesh in the blender to puree.  This works the blender pretty hard, so it’s not for a wimpy blender.  It works best if you start on a slow 
+    speed until the bottom part starts to puree, then gradually increase the speed until you get to the puree setting.  Let it run until it is 
+    really smooth.  At this point, you can use it for your favorite recipe or save it for later.  I usually pack 2 cups (most of my recipes call 
+    for a large can, which is pretty much 2 cups) in a pint size mason uar and freeze it.  You could actually can it yourself, but I use mine up 
+    before the holidays are over, so I don’t go to that much trouble.  It will keep for a few days in the fridge, but if you need it to last 
+    longer than that, just pop it in an airtight container or freezer bag and stick it in the freezer for a few months.    
+    2   3lb sugar pumpkins will yield about 5 pint jars of puree.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Mexican Cauliflower Rice Recipe',
+    '3 cups raw cauliflower rice
+    ½ yellow onion, diced
+    ½ cup chopped cilantro
+    1 cup finely diced zucchini (optional)
+    1 red bell pepper, diced (optional)
+    1 Tbsp fajita seasoning
+    1 Tbsp tomato paste
+    2 Tbsp olive oil', 
+    'Heat the oil in a large skillet on high heat. Add cauliflower, onion, zucchini, bell pepper and fajita seasoning and mix well.
+    Mix in the tomato paste until evenly distributed and then spread the rice evenly over the skillet and let cook until it starts to brown, 3-5 minutes.
+    Once the bottom starts to brown, stir the rice, even it out again and let bottom brown again. Repeat 3-4 times. The longer it cooks, the more quickly the bottom will brown.
+    Once it is cooked through, and browned to your liking, stir in the chopped cilantro and serve hot.
+    Enjoy!', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Glazed Carrots', 
+    '5 pounds carrots, boiled (baby ones from Costco)
+    2 cups chicken broth
+    Stick of butter
+    ½ cup sugar
+    Parsley Flakes', 
+    'Boil chicken broth and sugar, let it cook for 5 minutes. Reduce the broth until it is at a glaze consistency.  
+    The liquid will coat the back of a spoon.  Add the butter off heat and whisk it in.  Pour over steamed carrots.  
+    Sprinkle with parsley.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Funeral Potatoes', 
+    '2 lbs hash brown potatoes, or use the Costco dehydrated hash browns, and rehydrate enough to fill the pan when combined with the sauce.
+    1 Tblsp butter
+    1 chopped onion
+    2 cans cream of chicken soup
+    1 pint sour cream (can substitute some yogurt if needed)
+    ½ tsp salt
+    2 cups grated cheese
+    ½ cup melted butter', 
+    'Saute the onion in the Tblsp of butter till it’s softened.  Meanwhile, in a large bowl, combine the soup, sour cream, salt and melted butter.  
+    Stir in the cheese and then the hashbrown potatoes and sauteed onions. If desired, combine 4 Tblsp melted butter with 1 ½ cups crushed corn flakes 
+    and top the casserole.  Bake in a 9x13 pan at 350 degrees for 40-50 minutes.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('French Spring Soup', 
+    '¼ cup butter
+    1 pound leeks, chopped and rinsed
+    1 onion, chopped
+    2 quarts water
+    3 large potatoes chopped
+    2 large carrots chopped
+    1 bunch fresh asparagus, trimmed and cut into 1 inch pieces
+    1/3 cup uncooked white rice
+    4 tsp salt
+    ½ pound fresh spinach
+    1 cup heavy cream', 
+    'Melt butter in a large pot over medium heat.  Stir in the leeks and onion, and cook until tender.
+    Pour water into the pot. Mix in potatoes, carrots, asparagus, and rice. Season with salt. Bring to a boil, reduce heat, and simmer 30 minutes, until vegetables and rice are tender.  
+    Stir spinach and heavy cream into the soup mixture, and continue cooking about 5 minutes before serving. About 10 servings', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Mexican Cauliflower Rice Recipe', 
+    '3 cups raw cauliflower rice
+    ½ yellow onion, diced
+    ½ cup chopped cilantro
+    1 cup finely diced zucchini (optional)
+    1 red bell pepper, diced (optional)
+    1 Tbsp fajita seasoning
+    1 Tbsp tomato paste
+    2 Tbsp olive oil', 
+    'Heat the oil in a large skillet on high heat. Add cauliflower, onion, zucchini, bell pepper and fajita seasoning and mix well.
+    Mix in the tomato paste until evenly distributed and then spread the rice evenly over the skillet and let cook until it starts to brown, 3-5 minutes.
+    Once the bottom starts to brown, stir the rice, even it out again and let bottom brown again. Repeat 3-4 times. The longer it cooks, the more quickly the bottom will brown.
+    Once it is cooked through, and browned to your liking, stir in the chopped cilantro and serve hot.
+    Enjoy!', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Creamy Rosemary Potatoes', 
+    '4 tablespoons (1/2 stick) butter, plus softened butter, for the baking dish
+    5 to 6 medium russet potatoes
+    1 teaspoon salt, or to taste
+    Freshly ground black pepper
+    4 cloves garlic, finely chopped
+    1 medium onion, finely diced
+    One 8-ounce package cream cheese, at room temperature
+    1 cup half-and-half
+    2 tablespoons minced fresh rosemary
+    3 green onions, light green and medium green parts, thinly sliced
+    1 cup grated Parmesan
+    Special equipment: a madoline, optional', 
+    'Preheat the oven to 350 degrees F. Rub the bottom of a 9-by-13-inch baking dish with softened butter. Using a mandolin or a very sharp knife, 
+    slice the potatoes very thinly. Theyll cook better that way. Place the potatoes in the baking dish and sprinkle with 1/2 teaspoon salt and 
+    some pepper. In a large skillet over medium-low heat, melt the butter. Add the garlic and onions and cook until translucent, stirring occasionally, 
+    about 4 minutes. Cut the bar of cream cheese in half and add the halves to the skillet, stirring constantly. Continue stirring until the mixture is 
+    smooth and combined, about 3 minutes. Pour in the half-and-half and stir to combine. Mix in 1/2 teaspoon salt and some pepper. Taste and adjust the 
+    seasonings as necessary. Do not undersalt! Add the rosemary and green onions and stir to combine. Finally, mix in 1/2 cup of the grated Parmesan.
+    Pour the cream cheese mixture over the top of the potatoes. Scrape out the skillet to get every last drop. Sprinkle the remaining 1/2 cup grated 
+    Parmesan generously over the top and bake until golden brown and bubbly, at least 1 hour. Let stand for 10 minutes before serving.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Creamy Au Gratin Potatoes', 
+    ' 4 russet potatoes, sliced into 1/4 inch
+    slices
+    1 onion, sliced into rings
+    salt and pepper to taste
+    3 tablespoons butter
+    3 tablespoons all-purpose flour
+    1/2 teaspoon salt
+    2 cups milk
+    1 1/2 cups shredded Cheddar cheese', 
+    'Preheat oven to 400 degrees F (200 degrees C). Butter a 1 quart casserole dish.
+    Layer 1/2 of the potatoes into bottom of the prepared casserole dish. Top with the onion slices, and add the remaining potatoes. 
+    Season with salt and pepper to taste. In a medium-size saucepan, melt butter over medium heat. Mix in the flour and salt, and stir 
+    constantly with a whisk for one minute. Stir in milk. Cook until mixture has thickened. Stir in cheese all at once, and continue 
+    stirring until melted, about 30 to 60 seconds. Pour cheese over the potatoes, and cover the dish with aluminum foil.
+    Bake 1 1/2 hours in the preheated oven.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Cranberry-glazed carrots', 
+    '8 cups thinly sliced carrots
+    4 tablespoons bottled light raspberry or cranberry vinaigrette dressing
+    ½ cup canned whole cranberry sauce (or try a cran-raspberry or cran-orange sauce)
+    ¼ cup chopped toasted pecans', 
+    'Add carrots, dressing and cranberry sauce to a medium- to large-sized nonstick saucepan and bring to a boil.
+    Reduce heat to medium-low and cover pan. Simmer 8 to 10 minutes or until carrots are tender, stirring occasionally.
+    Spoon into serving bowl and sprinkle with pecans.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Corn on the Cob, Baked', 
+    'NA', 
+    'Bake corn on the cob, husks still on, right on the oven rack at 350 degrees for ½ hour.
+    Peel husks off and butter and enjoy!!', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Celery Oriental', 
+    'NA', 
+    'Slice 6-8 large, celery stalks on the diagonal
+    Cook them in a small amount of boiling salted water till just crisp-tender, drain.
+    Cook 1 cup sliced fresh mushrooms in 3 tablespoons (that much??) butter till tender.
+    Add the celery and ¼ cup toasted almond slices.  Toss lightly till hot.
+    4-6 servings', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Cauliflower Fried Rice', 
+    '1 head of cauliflower, (about 2½ cups chopped in a food processor)
+    2 Tbs sesame oil
+    1 small white onion, chopped
+    1 cup frozen peas and carrots, thawed
+    2-3 Tablespoons soy sauce (more or less to taste)
+    2 eggs, lightly beaten
+    2 Tbsp chopped green onions (optional)', 
+    'Rinse and chop the cauliflower into florets and put into a food processor. (If you dont have a food processor continue to chop the cauliflower into tiny pieces.) Pulse until the cauliflower until it is small and resembles rice.
+    Preheat a large skillet or wok to medium heat. Pour sesame oil in the bottom. Add white onion and peas and carrots and fry until tender.
+    Slide the onion, peas and carrots to the side, and pour the beaten eggs onto the other side. Using a spatula, scramble the eggs. Once cooked, mix the eggs with the vegetable mix.
+    Add the cauliflower to the veggie and egg mixture. Pour the soy sauce on top. Stir and fry the rice and veggie mixture until heated through and cauliflower is tender. Add chopped green onions if desired.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Brussel Sprout Recipes', 
+    'NA', 
+    'Preheat oven to 400°F. Toss Brussels sprouts with oil, salt and pepper on a rimmed baking sheet and roast, 
+    stirring once or twice, until deep golden brown, crisp outside and tender inside, 30 to 35 minutes. The leaves 
+    that are loose will be especially brown and crispy. Transfer to a bowl and serve.
+    
+    Rosemary Parmesan Brussels Sprouts
+    Add 1 tablespoon chopped fresh rosemary to Brussels sprouts before roasting. During the last 5 minutes of roasting, add 1/4 cup pine nuts. 
+    Stir well and continue roasting until Brussels sprouts are tender. Before serving, toss with 1/4 cup shredded parmesan cheese.
+
+    Cranberry Pecan Brussels Sprouts
+    During the last 5 minutes of roasting, add 1 cup dried cranberries and 1/4 cup pecan pieces. Stir well and continue roasting until Brussels 
+    sprouts are tender.
+
+    Brussels Sprouts and Kale Salad
+    After roasting, allow Brussels sprouts to cool to room temperature. Toss with 4 cups baby kale mix, 1/4 cup crumbled goat cheese and 1/4 cup 
+    balsamic vinaigrette.', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Brussel Sprout Chips', 
+    'NA', 
+    'Cut off the base of the rinsed brussel sprouts and separate the leaves till you no longer can.
+    Save the insides for roasting later.
+    Pour leaves into a bowl and mix a little oil into the leaves.
+    Sprinkle with salt and pepper to taste, possibly a little parmesan cheese.
+    Toss and pour onto a greased cookie sheet.
+    Bake at 350 for 10 mins, then lower heat to 250 and continue to bake for another 10 mins.
+    Serve and enjoy :)', 2);
+
+    INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Amazing Mashed Potatoes', 
+    '5 lbs of potatoes, russets or yellow gold (haven’t tried golds yet)  peeled and cut up
+    Put in pot and cover with salted water and boil till tender, then drain and add:
+    ½ cup (cube) of melted butter
+    About ½ cup sour cream
+    1 tsp salt
+    ½ tsp pepper
+    A few dashes of Johnny’s seasoning', 
+    'Mash with a masher till smooth, but don’t overbeat or it will get gluey.', 2);
