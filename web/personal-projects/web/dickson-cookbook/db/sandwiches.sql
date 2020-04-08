1bdc8bf721a926769e5779f101d57f86bc95d8da
Jordan Dickson <jfdickson25@gmail.com>
2020-04-07 20:33:13 -0600

    Setting up sending the details to the next page

---
+++ web/personal-projects/web/dickson-cookbook/db/sandwiches.sql
@@ -0,0 +1,37 @@
+INSERT INTO category (category_name) VALUES ('Sandwiches');
+  
+  INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('Billie Joys  RECIPE for SLOPPY JOES', 
+    '1 pound lean ground hamburger
+    ¼ cup chopped onion
+    ½ tsp garlic powder
+    1 tsp prepared yellow mustard
+    ¾ cup ketchup
+    3 tsp brown sugar
+    salt & pepper to taste if desired
+    Brown the ground beef and onion together.  Stir in remaining ingredients and simmer for 30 minutes.  Season with salt and pepper.
+    1/6th recipe 170 calories
+    1/4th recipe 250 calories', 
+    'Mom’s Sloppy Joes (Recipe from Julie)
+    3 lbs hamburger
+    2 yellow onions chopped
+    2 red bell peppers chopped
+    3 cups ketchup
+    ¼ cup + 2 Tblsp of each of the following:
+    Apple Cider Vinegar
+    Worcestershire Sauce
+    Brown Sugar
+    Yellow Mustard
+    Hickory Flavored BBQ Sauce
+    ¼ cup Parmesan Cheese
+
+    Small Recipe:
+    1 lb hamburger
+    ½ to ¾ of a yellow onion diced
+    1 red bell pepper diced
+    1 cup or less ketchup
+    2 Tblsp apple cider vinegar
+    2 Tblsp worcestershire sauce
+    2 Tblsp brown sugar
+    2 Tblsp yellow mustard
+    2 Tblsp hickory flavored BBQ sauce
+    1 ½ Tblsp parmesan cheese', 5);
---
+++ web/personal-projects/web/dickson-cookbook/db/sauces-dips-and-condiments.sql
@@ -0,0 +1,12 @@
+/* Sauces, Dips and Condiments */
+  INSERT INTO category (category_name) VALUES ('Sauces, Dips and Condiments');
+  
+  INSERT INTO recipe (title, ingredients, instructions, category_id) VALUES ('White Bean Guacamole', 
+    'NA', 
+    '2 lightly packed cups avocado
+    1 cup cooked white beans
+    ½ tsp salt
+    2- 2 ½ Tblsp lemon juice
+    Water to thin
+    Blend
+    ', 4);
